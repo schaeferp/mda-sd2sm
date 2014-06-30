7288adcbbb6bfa5094f66deb2d98537b0e84c84a
Philip Schäfer <phschaef@googlemail.com>
2014-06-30 10:51:13 +0200

    Moved Receiver-property from OperationInvocation to Operation

--- 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Core/Domain/Operation.cs
+++ 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Core/Domain/Operation.cs
@@ -1,8 +1,11 @@
 ﻿namespace Egp.Mda.Transformation.Core
 {
+    public enum OperationSort { Reply , Request };
+
     public class Operation
     {
-        public string Guid { get; set; }
         public string Name { get; set; }
+        public Actor Receiver { get; set; }
+        public OperationSort Sort { get; set; }
     }
 }
--- 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Core/Domain/OperationInvocation.cs
+++ 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Core/Domain/OperationInvocation.cs
@@ -5,7 +5,6 @@
         public StateInvariant PreStateInvariant { get; set; }
         public StateInvariant PostStateInvariant { get; set; }
         public Actor Sender { get; set; }
-        public Actor Receiver { get; set; }
         public Operation Operation { get; set; }
         public ReturnType Return { get; set; }
     }
