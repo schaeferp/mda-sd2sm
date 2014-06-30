c7b73222f59d13040a626a2f73ec16c8d4a9ca82
Philip Schäfer <phschaef@googlemail.com>
2014-06-30 10:52:02 +0200

    Direct access to Invocations-property instead of lazy field init

--- 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Core/Scenario/Scenario.cs
+++ 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Core/Scenario/Scenario.cs
@@ -4,13 +4,8 @@ namespace Egp.Mda.Transformation.Core
 {
     public class Scenario
     {
-        private IEnumerable<OperationInvocation> _invocations;
-
         public string Name { get; set; }
 
-        public IEnumerable<OperationInvocation> Invocations
-        {
-            get { return _invocations ?? (_invocations = new List<OperationInvocation>()); }
-        }
+        public IEnumerable<OperationInvocation> Invocations { get; set; }
     }
 }
