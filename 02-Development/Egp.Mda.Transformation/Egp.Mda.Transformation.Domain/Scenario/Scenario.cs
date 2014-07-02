7661e13149ee669325c556837ead64dfd2b3cc4c
Philip Schäfer <phschaef@googlemail.com>
2014-07-02 19:15:15 +0200

    Overwrite ToString + lazy collection init

--- 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Domain/Scenario/Scenario.cs
+++ 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Domain/Scenario/Scenario.cs
@@ -4,8 +4,18 @@ namespace Egp.Mda.Transformation.Domain.Scenario
 {
     public class Scenario
     {
+        private IList<OperationInvocation> _invocations;
+
         public string Name { get; set; }
 
-        public IList<OperationInvocation> Invocations { get; set; }
+        public IList<OperationInvocation> Invocations
+        {
+            get { return _invocations ?? (_invocations = new List<OperationInvocation>()); }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
