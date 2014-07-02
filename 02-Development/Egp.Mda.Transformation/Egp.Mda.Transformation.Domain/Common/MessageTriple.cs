5ae12fc6a4e4d11747952db04c04275479960e14
Philip Schäfer <phschaef@googlemail.com>
2014-07-02 20:24:02 +0200

    Add from renaming. foo.

---
+++ 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Domain/Common/MessageTriple.cs
@@ -0,0 +1,11 @@
+﻿namespace Egp.Mda.Transformation.Domain.Common
+{
+    public class MessageTriple
+    {
+        public IParticipant Target { get; set; }
+
+        public string Operation { get; set; }
+
+        public string Return { get; set; }
+    }
+}
