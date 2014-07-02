b144d86702cfb1c1e4b04c9bfeae926949906a46
Philip Schäfer <phschaef@googlemail.com>
2014-07-02 20:02:19 +0200

    Refactored ScenarioService.cs

--- 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Core/Scenario/ScenarioService.cs
+++ 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Core/Scenario/ScenarioService.cs
@@ -11,8 +11,10 @@ namespace Egp.Mda.Transformation.Core
     {
         private const string UmlCollaborationAttributeValue = "uml:Collaboration";
         private const string ReplySortValue = "reply";
+
         private const string UmlMessageOccurenceSpecificationAttributeValue =
             "uml:MessageOccurrenceSpecification";
+
         private const string UmlStateInvariantAttributeValue = "uml:StateInvariant";
 
         private XmiSequenceDiagramModel _xmiModel;
@@ -24,106 +26,132 @@ namespace Egp.Mda.Transformation.Core
             var collaborations =
                 xmiModel.PackagedElements.Values.Where(e => e.XmiType == UmlCollaborationAttributeValue).ToList();
             var result = new List<Scenario>(collaborations.Count);
-            foreach (var sequenceDiagram in collaborations)
+            result.AddRange(collaborations.Select(CreateScenarioFrom));
+            return result;
+        }
+
+        private Scenario CreateScenarioFrom(PackagedElement sequenceDiagram)
+        {
+            var scenario = new Scenario
             {
-                var scenario = new Scenario
+                Name = sequenceDiagram.Name
+            };
+            var lastInvocationPerParticipant = new Dictionary<IParticipant, OperationInvocation>();
+            foreach (var fragment in sequenceDiagram.Fragments.Values)
+            {
+                switch (fragment.XmiType)
                 {
-                    Name = sequenceDiagram.Name
-                };
-                result.Add(scenario);
-                var lastInvocationPerParticipant = new Dictionary<IParticipant, OperationInvocation>();
-                foreach (var fragment in sequenceDiagram.Fragments.Values)
-                {
-                    switch (fragment.XmiType)
+                    case UmlStateInvariantAttributeValue: AddStateInvariantToInvocations(fragment, sequenceDiagram, lastInvocationPerParticipant);
+                        break;
+                    case UmlMessageOccurenceSpecificationAttributeValue:
                     {
-                        case UmlStateInvariantAttributeValue:
-                        {
-                            var state = new StateInvariant() {Name = fragment.Body};
-                            foreach (var lifelineId in fragment.Covered)
-                            {
-                                var lifeline = LookupLifelineFor(sequenceDiagram, lifelineId);
-                                var participant = LookupParticipantFor(sequenceDiagram, lifeline);
-                                OperationInvocation activeInvocation;
-                                var activeInvocationExists = lastInvocationPerParticipant.TryGetValue(participant,
-                                    out activeInvocation);
-                                if (activeInvocationExists) activeInvocation.PostStateInvariant = state;
-                                var newInvocation = new OperationInvocation() {PreStateInvariant = state};
-                                lastInvocationPerParticipant.Remove(participant);
-                                lastInvocationPerParticipant.Add(participant, newInvocation);
-                            }
-                            break;
-                        }
-                        case UmlMessageOccurenceSpecificationAttributeValue:
-                        {
-                            var message = LookupMessageFor(sequenceDiagram, fragment.Message);
-                            var participantIsSender = message.SendEvent == fragment.XmiId;
-                            if (participantIsSender) continue;
-
-                            foreach (var lifelineId in fragment.Covered)
-                            {
-                                var lifeline = LookupLifelineFor(sequenceDiagram, lifelineId);
-                                var participant = LookupParticipantFor(sequenceDiagram, lifeline);
-                                OperationInvocation lastInvocation;
-                                var lastInvocationExists = lastInvocationPerParticipant.TryGetValue(participant,
-                                    out lastInvocation);
-                                if (lastInvocationExists)
-                                {
-                                    var state = StateInvariant.CreateAnonymous();
-                                    var lastInvocationHasOperation = lastInvocation.Operation != null;
-                                    var messageIsReply = message.Sort == ReplySortValue;
-                                    if (lastInvocationHasOperation && !messageIsReply)
-                                    {
-                                        lastInvocation.PostStateInvariant = state;
-                                        var newInvocation = new OperationInvocation()
-                                        {
-                                            PreStateInvariant = state
-                                        };
-                                        lastInvocationPerParticipant[participant] = newInvocation;
-                                        lastInvocation = newInvocation;
-                                    }
-
-                                    if (messageIsReply)
-                                    {
-                                        lastInvocation.Return = message.Name;
-                                    }
-                                    else
-                                    {
-                                        lastInvocation.Operation = new Operation()
-                                        {
-                                            Name = message.Name,
-                                            Receiver = participant,
-                                        };
-                                        scenario.Invocations.Add(lastInvocation);
-                                    }
-                                }
-                                else
-                                {
-                                    var preState = StateInvariant.CreateAnonymous();
-                                    var operation = new Operation()
-                                    {
-                                        Name = message.Name,
-                                        Receiver = participant,
-                                    };
-                                    var invocation = new OperationInvocation()
-                                    {
-                                        PreStateInvariant = preState,
-                                        Operation = operation
-                                    };
-                                    lastInvocationPerParticipant.Add(participant, invocation);
-                                    scenario.Invocations.Add(invocation);
-                                }
-                            }
-                            break;
-                        }
+                        OperationInvocation lastInvocation;
+                        AddMessageToInvocations(fragment, sequenceDiagram, lastInvocationPerParticipant, scenario);
+                        break;
                     }
                 }
-                foreach (var lastInvocation in lastInvocationPerParticipant.Values)
+            }
+            AddPostStatesToLastInvocations(lastInvocationPerParticipant);
+            return scenario;
+        }
+
+        private void AddMessageToInvocations(Fragment fragment, PackagedElement sequenceDiagram, Dictionary<IParticipant, OperationInvocation> lastInvocationPerParticipant, Scenario scenario)
+        {
+            var message = LookupMessageFor(sequenceDiagram, fragment.Message);
+            var participantIsSender = message.SendEvent == fragment.XmiId;
+            if (participantIsSender) return;
+
+            foreach (var lifelineId in fragment.Covered)
+            {
+                var lifeline = LookupLifelineFor(sequenceDiagram, lifelineId);
+                var participant = LookupParticipantFor(sequenceDiagram, lifeline);
+                OperationInvocation lastInvocation;
+                var lastInvocationExists = lastInvocationPerParticipant.TryGetValue(participant, out lastInvocation);
+                if (lastInvocationExists)
                 {
-                    if (null == lastInvocation.PostStateInvariant)
-                        lastInvocation.PostStateInvariant = StateInvariant.CreateAnonymous();
+                    UpdateCurrentParticipantInvocation(message, lastInvocationPerParticipant, participant,lastInvocation, scenario);
+                }
+                else
+                {
+                    CreateCurrentParticipantInvocation(message, lastInvocationPerParticipant, participant, scenario);
                 }
             }
-            return result;
+        }
+
+        private void UpdateCurrentParticipantInvocation(Message message, Dictionary<IParticipant, OperationInvocation> lastInvocationPerParticipant, IParticipant participant, OperationInvocation lastInvocation, Scenario scenario)
+        {
+            var state = StateInvariant.CreateAnonymous();
+            var lastInvocationHasOperation = lastInvocation.Operation != null;
+            var messageIsReply = message.Sort == ReplySortValue;
+            if (lastInvocationHasOperation && !messageIsReply)
+            {
+                lastInvocation.PostStateInvariant = state;
+                var newInvocation = new OperationInvocation()
+                {
+                    PreStateInvariant = state
+                };
+                lastInvocationPerParticipant[participant] = newInvocation;
+                lastInvocation = newInvocation;
+            }
+
+            if (messageIsReply)
+            {
+                lastInvocation.Return = message.Name;
+            }
+            else
+            {
+                lastInvocation.Operation = new Operation()
+                {
+                    Name = message.Name,
+                    Receiver = participant,
+                };
+                scenario.Invocations.Add(lastInvocation);
+            }
+        }
+
+        private void CreateCurrentParticipantInvocation(Message message, Dictionary<IParticipant, OperationInvocation> lastInvocationPerParticipant, IParticipant participant, Scenario scenario)
+        {
+            var preState = StateInvariant.CreateAnonymous();
+            var operation = new Operation()
+            {
+                Name = message.Name,
+                Receiver = participant,
+            };
+            var invocation = new OperationInvocation()
+            {
+                PreStateInvariant = preState,
+                Operation = operation
+            };
+            lastInvocationPerParticipant.Add(participant, invocation);
+            scenario.Invocations.Add(invocation);
+        }
+
+        private void AddStateInvariantToInvocations(Fragment fragment, PackagedElement sequenceDiagram, Dictionary<IParticipant, OperationInvocation> lastInvocationPerParticipant)
+        {
+            var state = new StateInvariant() { Name = fragment.Body };
+            foreach (var lifelineId in fragment.Covered)
+            {
+                var lifeline = LookupLifelineFor(sequenceDiagram, lifelineId);
+                var participant = LookupParticipantFor(sequenceDiagram, lifeline);
+                OperationInvocation activeInvocation;
+                var activeInvocationExists = lastInvocationPerParticipant.TryGetValue(participant,
+                    out activeInvocation);
+                if (activeInvocationExists) activeInvocation.PostStateInvariant = state;
+                var newInvocation = new OperationInvocation() { PreStateInvariant = state };
+                lastInvocationPerParticipant.Remove(participant);
+                lastInvocationPerParticipant.Add(participant, newInvocation);
+            }
+        }
+
+        private void AddPostStatesToLastInvocations(
+            Dictionary<IParticipant, OperationInvocation> lastInvocationPerParticipant)
+        {
+            var invocationsWithoutPostState = lastInvocationPerParticipant.Values.Where(
+                lastInvocation => null == lastInvocation.PostStateInvariant);
+            foreach (var lastInvocation in invocationsWithoutPostState)
+            {
+                lastInvocation.PostStateInvariant = StateInvariant.CreateAnonymous();
+            }
         }
 
         private void Init(XmiSequenceDiagramModel xmiModel)
