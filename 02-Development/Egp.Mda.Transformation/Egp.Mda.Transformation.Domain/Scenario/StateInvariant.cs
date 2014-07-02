fd30982a30b50a9ea37ab908e2eccedfccc81381
Philip Schäfer <phschaef@googlemail.com>
2014-07-02 19:15:39 +0200

    Overwrite ToString + Added CreateAnonymous

--- 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Domain/Scenario/StateInvariant.cs
+++ 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Domain/Scenario/StateInvariant.cs
@@ -1,7 +1,19 @@
-﻿namespace Egp.Mda.Transformation.Domain.Scenario
+﻿using System;
+
+namespace Egp.Mda.Transformation.Domain.Scenario
 {
     public class StateInvariant
     {
         public string Name { get; set; }
+
+        public static StateInvariant CreateAnonymous()
+        {
+            return new StateInvariant {Name = Guid.NewGuid().ToString()};
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
