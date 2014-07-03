204f4ff67b724b3eeb44bfe854e00889c2200f3a
Florian <florian.jaeger@student.uni-augsburg.de>
2014-07-03 20:17:03 +0200

    added FileWriting

---
+++ 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Core/Output/FileWriter.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Egp.Mda.Transformation.Core.Output
+{
+    internal class FileWriter : IWriter
+    {
+        public void write(string text)
+        {
+            var strTempFile = Path.GetTempFileName();
+            File.WriteAllText(@strTempFile, text);
+        }
+
+        public void write(string uri, string text)
+        {
+            File.WriteAllText(@uri, text);
+        }
+    }
+}
---
+++ 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Core/Output/IWriter.cs
@@ -0,0 +1,8 @@
+﻿namespace Egp.Mda.Transformation.Core.Output
+{
+    internal interface IWriter
+    {
+        void write(string text);
+        void write(string uri, string text); 
+    }
+}
