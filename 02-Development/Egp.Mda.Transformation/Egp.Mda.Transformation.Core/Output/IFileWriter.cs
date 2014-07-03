e462eba1e5d9ce5ce06d8e329405c70547387e63
Florian <florian.jaeger@student.uni-augsburg.de>
2014-07-03 19:23:15 +0200

    empty file

---
+++ 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Core/Output/IFileWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egp.Mda.Transformation.Core.Output
+{
+    interface IFileWriter
+    {
+    }
+}
