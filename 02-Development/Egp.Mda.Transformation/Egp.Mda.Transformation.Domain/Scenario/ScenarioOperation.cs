015d56548e067dbf0aa64b9fde4359ec181b1a30
Philip Schäfer <phschaef@googlemail.com>
2014-07-03 15:33:58 +0200

    Double-linking of ScnOp and Participant

--- 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Domain/Scenario/ScenarioOperation.cs
+++ 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Domain/Scenario/ScenarioOperation.cs
@@ -2,8 +2,25 @@
 {
     public class ScenarioOperation
     {
+        private IParticipant _receiver;
         public string Name { get; set; }
-        public IParticipant Receiver { get; set; }
+
+        public IParticipant Receiver
+        {
+            get { return _receiver; }
+            set
+            {
+                if (null != _receiver)
+                {
+                    _receiver.Operations.Remove(this);
+                }
+                _receiver = value;
+                if (null != value)
+                {
+                    _receiver.Operations.Add(this);
+                }
+            }
+        }
 
         public override string ToString()
         {
