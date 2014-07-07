cc3092683b6d8d4dfb6a5929a5593084ab964f35
Philip Schäfer <phschaef@googlemail.com>
2014-07-07 23:58:30 +0200

    Doc

--- 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Core/Scenario/ScenarioService.cs
+++ 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Core/Scenario/ScenarioService.cs
@@ -7,18 +7,21 @@ namespace Egp.Mda.Transformation.Core
 {
     public class ScenarioService : IScenarioService
     {
+        #region XML Attribute Value Constants
+
         private const string UmlCollaborationAttributeValue = "uml:Collaboration";
-        private const string ReplySortValue = "reply";
+        private const string ReplySortAttributeValue = "reply";
 
         private const string UmlMessageOccurenceSpecificationAttributeValue =
             "uml:MessageOccurrenceSpecification";
 
         private const string UmlStateInvariantAttributeValue = "uml:StateInvariant";
-        private IParticipant _lastSender;
 
+        #endregion
+
+        private IParticipant _lastSender;
         private IDictionary<IParticipant, IDictionary<string, ScenarioOperation>> _participantOperations;
         private IDictionary<Lifeline, IParticipant> _participants;
-
         private XmiSequenceDiagramModel _xmiModel;
 
         public ScenarioModel From(XmiSequenceDiagramModel xmiModel)
@@ -31,6 +34,12 @@ namespace Egp.Mda.Transformation.Core
             return result;
         }
 
+        /// <summary>
+        ///     Creates a <see cref="Scenario" /> based on the given <see cref="PackagedElement" /> object representing
+        ///     an entire, single sequence diagram.
+        /// </summary>
+        /// <param name="sequenceDiagram">The sequence diagram's root node.</param>
+        /// <returns>A corresponing scenario.</returns>
         private Scenario CreateScenarioFrom(PackagedElement sequenceDiagram)
         {
             var scenario = new Scenario
@@ -52,16 +61,24 @@ namespace Egp.Mda.Transformation.Core
                     }
                 }
             }
-            AddPostStatesToLastInvocations(lastInvocationPerParticipant);
+            AddAnonymousPostStatesToPostStateLessInvocations(lastInvocationPerParticipant);
             return scenario;
         }
 
+        /// <summary>
+        ///     Evaluates the Message being described by the given Fragment and enriches both the given invocation dictionary
+        ///     and scenario appropriately.
+        /// </summary>
+        /// <param name="fragment">The Fragment describing the Message to add.</param>
+        /// <param name="sequenceDiagram">The PackagedElement describing the containing sequence diagram.</param>
+        /// <param name="lastInvocationPerParticipant">The invocation tracker.</param>
+        /// <param name="scenario">The scenario to enrich.</param>
         private void AddMessageToInvocations(Fragment fragment, PackagedElement sequenceDiagram,
-            Dictionary<IParticipant, ScenarioOperationInvocation> lastInvocationPerParticipant, Scenario scenario)
+            IDictionary<IParticipant, ScenarioOperationInvocation> lastInvocationPerParticipant, Scenario scenario)
         {
             var message = LookupMessageFor(sequenceDiagram, fragment.Message);
             var participantIsSender = message.SendEvent == fragment.XmiId;
-            var messageIsReply = message.Sort == ReplySortValue;
+            var messageIsReply = message.Sort == ReplySortAttributeValue;
 
             foreach (var lifelineId in fragment.Covered)
             {
@@ -77,13 +94,14 @@ namespace Egp.Mda.Transformation.Core
                     _lastSender = participant;
                     continue;
                 }
-                
+
                 if (messageIsReply) continue;
 
                 ScenarioOperationInvocation newInvocation;
                 if (lastInvocationExists)
                 {
-                    newInvocation = UpdateCurrentParticipantInvocation(message, lastInvocationPerParticipant, participant,
+                    newInvocation = UpdateCurrentParticipantInvocation(message, lastInvocationPerParticipant,
+                        participant,
                         lastInvocation, scenario);
                 }
                 else
@@ -95,8 +113,18 @@ namespace Egp.Mda.Transformation.Core
             }
         }
 
+        /// <summary>
+        ///     Updates the Invocation of the given Participant being currently evaluated.
+        /// </summary>
+        /// <param name="message">The Message to derive an Operation for the current Invocation from.</param>
+        /// <param name="lastInvocationPerParticipant">The Invocation tracker.</param>
+        /// <param name="participant">The corresponding Participant.</param>
+        /// <param name="lastInvocation">The last Invocation that occurred in here.</param>
+        /// <param name="scenario">The containing Scenario.</param>
+        /// <returns></returns>
         private ScenarioOperationInvocation UpdateCurrentParticipantInvocation(Message message,
-            Dictionary<IParticipant, ScenarioOperationInvocation> lastInvocationPerParticipant, IParticipant participant,
+            IDictionary<IParticipant, ScenarioOperationInvocation> lastInvocationPerParticipant,
+            IParticipant participant,
             ScenarioOperationInvocation lastInvocation, Scenario scenario)
         {
             var state = ScenarioStateInvariant.CreateAnonymous();
@@ -118,8 +146,17 @@ namespace Egp.Mda.Transformation.Core
             return lastInvocation;
         }
 
+        /// <summary>
+        ///     Creates a new Invocation for the given Participant sending the given Message.
+        /// </summary>
+        /// <param name="message">The Message to derive an Operation from.</param>
+        /// <param name="lastInvocationPerParticipant">The invocation tracker to update.</param>
+        /// <param name="participant">The corresponding Participant.</param>
+        /// <param name="scenario">The scenario to add the invocation to.</param>
+        /// <returns>The created Invocation.</returns>
         private ScenarioOperationInvocation CreateCurrentParticipantInvocation(Message message,
-            Dictionary<IParticipant, ScenarioOperationInvocation> lastInvocationPerParticipant, IParticipant participant,
+            IDictionary<IParticipant, ScenarioOperationInvocation> lastInvocationPerParticipant,
+            IParticipant participant,
             Scenario scenario)
         {
             var preState = ScenarioStateInvariant.CreateAnonymous();
@@ -134,8 +171,15 @@ namespace Egp.Mda.Transformation.Core
             return invocation;
         }
 
+        /// <summary>
+        ///     Adds the state invariant decribed by the given Fragment, within the sequence diagram described by the
+        ///     given PackagedElement, to the Invocation dictionary.
+        /// </summary>
+        /// <param name="fragment">The Fragment describing the state invariant.</param>
+        /// <param name="sequenceDiagram">The PackagedElement describing the sequence diagram.</param>
+        /// <param name="lastInvocationPerParticipant">The invocations to update.</param>
         private void AddStateInvariantToInvocations(Fragment fragment, PackagedElement sequenceDiagram,
-            Dictionary<IParticipant, ScenarioOperationInvocation> lastInvocationPerParticipant)
+            IDictionary<IParticipant, ScenarioOperationInvocation> lastInvocationPerParticipant)
         {
             var state = new ScenarioStateInvariant {Name = fragment.Body};
             foreach (var lifelineId in fragment.Covered)
@@ -152,7 +196,12 @@ namespace Egp.Mda.Transformation.Core
             }
         }
 
-        private void AddPostStatesToLastInvocations(
+        /// <summary>
+        ///     Adds an anonymous post state to each invocation within the given dictionary, that does not
+        ///     yet have any post state.
+        /// </summary>
+        /// <param name="lastInvocationPerParticipant">The invocations to update.</param>
+        private static void AddAnonymousPostStatesToPostStateLessInvocations(
             Dictionary<IParticipant, ScenarioOperationInvocation> lastInvocationPerParticipant)
         {
             var invocationsWithoutPostState = lastInvocationPerParticipant.Values.Where(
@@ -163,7 +212,14 @@ namespace Egp.Mda.Transformation.Core
             }
         }
 
-
+        /// <summary>
+        ///     Creates or looks up the Operation identified by the given operation name and the given Participant.
+        ///     If the given Operation already exists for the given Particpant, it will be returned.
+        ///     If it does not yet exist, it will be created, stored and returned.
+        /// </summary>
+        /// <param name="participant">The Participant executing the Operation.</param>
+        /// <param name="operationName">The required Operation's name.</param>
+        /// <returns>A corresponding Scenario Operation.</returns>
         private ScenarioOperation CreateOrLookOperation(IParticipant participant, String operationName)
         {
             IDictionary<string, ScenarioOperation> participantOperations;
@@ -186,6 +242,10 @@ namespace Egp.Mda.Transformation.Core
             return operation;
         }
 
+        /// <summary>
+        ///     Field initialization.
+        /// </summary>
+        /// <param name="xmiModel">The input sequence diagram model.</param>
         private void Init(XmiSequenceDiagramModel xmiModel)
         {
             _xmiModel = xmiModel;
@@ -193,7 +253,14 @@ namespace Egp.Mda.Transformation.Core
             _participantOperations = new Dictionary<IParticipant, IDictionary<string, ScenarioOperation>>();
         }
 
-        private Lifeline LookupLifelineFor(PackagedElement sequenceDiagram, string lifelineId)
+        /// <summary>
+        ///     Looks up and returns a Lifeline within a given <see cref="PackagedElement" /> describing a sequence diagram
+        ///     and identified by the given lifeline id.
+        /// </summary>
+        /// <param name="sequenceDiagram">The PackagedElement describing the containing sequence diagram.</param>
+        /// <param name="lifelineId">The required Lifeline's id.</param>
+        /// <returns>A corresponding lifeline.</returns>
+        private static Lifeline LookupLifelineFor(PackagedElement sequenceDiagram, string lifelineId)
         {
             Lifeline lifeline;
             var exists = sequenceDiagram.Lifelines.TryGetValue(lifelineId, out lifeline);
@@ -201,6 +268,13 @@ namespace Egp.Mda.Transformation.Core
             return lifeline;
         }
 
+        /// <summary>
+        ///     Looksup and returns a Participant within a given <see cref="PackagedElement" /> describing a sequence diagram
+        ///     and identified by the given lifeline.
+        /// </summary>
+        /// <param name="sequenceDiagram">The PackagedElement describing the containing sequence diagram.</param>
+        /// <param name="lifeline">The lifeline identifing the required Participant.</param>
+        /// <returns>A correponding Participant.</returns>
         private IParticipant LookupParticipantFor(PackagedElement sequenceDiagram, Lifeline lifeline)
         {
             IParticipant participant;
@@ -222,7 +296,14 @@ namespace Egp.Mda.Transformation.Core
             return participant;
         }
 
-        private Message LookupMessageFor(PackagedElement sequenceDiagram, string messageId)
+        /// <summary>
+        ///     Looks up and returns a Message within a given <see cref="PackagedElement" /> describing a sequence diagram
+        ///     and identified by the given message id.
+        /// </summary>
+        /// <param name="sequenceDiagram">The PackagedElement describing the containing sequence diagram.</param>
+        /// <param name="messageId">The required message's id.</param>
+        /// <returns>A corresponding Message.</returns>
+        private static Message LookupMessageFor(PackagedElement sequenceDiagram, string messageId)
         {
             Message message;
             var exists = sequenceDiagram.Messages.TryGetValue(messageId, out message);
