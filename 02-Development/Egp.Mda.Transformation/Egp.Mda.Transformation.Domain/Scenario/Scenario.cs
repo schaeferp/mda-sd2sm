67bbca6cadf151c58d369fd473740319aa84c571
Manuel Gerding <me@manuel-gerding.de>
2014-07-07 22:05:50 +0200

    Documentation of Scenario Domain (Transformation.Domain/Scenario/*)

--- 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Domain/Scenario/Scenario.cs
+++ 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Domain/Scenario/Scenario.cs
@@ -3,27 +3,48 @@ using System.Linq;
 
 namespace Egp.Mda.Transformation.Domain
 {
+    /// <summary>
+    ///     One scenario which may for instance represent a sequence diagram or another dynamic behavior.
+    /// </summary>
     public class Scenario
     {
         private IList<ScenarioOperationInvocation> _invocations;
 
+        /// <summary>
+        ///     The name of the scenario.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        ///     A ordered collection of invocations. The order specifies the order of operation invocations.
+        /// </summary>
         public IList<ScenarioOperationInvocation> Invocations
         {
             get { return _invocations ?? (_invocations = new List<ScenarioOperationInvocation>()); }
         }
 
+        /// <summary>
+        ///     Distinct collection of all <see cref="IParticipant" />s which receives an operation invocation in this scenario.
+        ///     Distinction is based on object reference.
+        /// </summary>
         public IEnumerable<IParticipant> ReceiverParticipants
         {
             get { return Invocations.Select(i => i.ScenarioOperation.Receiver).Distinct(); }
         }
 
+        /// <summary>
+        ///     Distinct collection of all <see cref="IParticipant" />s which sends an operation invocation in this scenario.
+        ///     Distinction is based on object reference.
+        /// </summary>
         public IEnumerable<IParticipant> SenderParticipants
         {
             get { return Invocations.Select(i => i.Sender).Distinct(); }
         }
 
+        /// <summary>
+        ///     Distinct collection of all <see cref="IParticipant" />s which receives or sends an operation invocation in this
+        ///     scenario. Distinction is based on object reference.
+        /// </summary>
         public IEnumerable<IParticipant> Participants
         {
             get { return ReceiverParticipants.Union(SenderParticipants).Distinct(); }
--- 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Domain/Scenario/ScenarioOperation.cs
+++ 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Domain/Scenario/ScenarioOperation.cs
@@ -1,10 +1,20 @@
 ﻿namespace Egp.Mda.Transformation.Domain
 {
+    /// <summary>
+    /// An operation which can be invoked on the <see cref="Receiver"/>
+    /// </summary>
     public class ScenarioOperation
     {
         private IParticipant _receiver;
+
+        /// <summary>
+        /// The name of the operation.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// The <see cref="IParticipant"/> which provides this operation for invocation (see <see cref="ScenarioOperationInvocation"/>).
+        /// </summary>
         public IParticipant Receiver
         {
             get { return _receiver; }
