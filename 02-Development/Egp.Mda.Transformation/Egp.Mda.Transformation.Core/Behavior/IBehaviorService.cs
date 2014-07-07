a14f9ea59c0c28b127609bb18ce19722bacbeeae
Manuel Gerding <me@manuel-gerding.de>
2014-07-07 22:04:57 +0200

    Documentation of BehaviorService (IBehaviorService.cs and BehaviorService.cs)

--- 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Core/Behavior/IBehaviorService.cs
+++ 02-Development/Egp.Mda.Transformation/Egp.Mda.Transformation.Core/Behavior/IBehaviorService.cs
@@ -4,6 +4,15 @@ namespace Egp.Mda.Transformation.Core
 {
     public interface IBehaviorService
     {
+        /// <summary>
+        ///     Responsible for transforming the given <paramref name="scenarioModel" /> into the appropriate
+        ///     <see cref="BehaviorModel" />.
+        ///     The ScenarioModel represents the various scenarios which are represented originally via sequence diagrams.
+        ///     <seealso cref="BehaviorModel" />
+        ///     <seealso cref="ScenarioModel" />
+        /// </summary>
+        /// <param name="scenarioModel">scenarioModel which should be transformed into a behavior model</param>
+        /// <returns>behaviorModel which results from the given <paramref name="scenarioModel"/></returns>
         BehaviorModel From(ScenarioModel scenarioModel);
     }
 }
